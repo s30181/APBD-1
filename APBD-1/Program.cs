b14ba74091f4e66ec3051846c6cdaeb9a59f5ba1
s30181 <s30181@pjwstk.edu.pl>
2025-03-03 18:34:46 +0100

    Added CalculateAverage

--- APBD-1/Program.cs
+++ APBD-1/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
 
-Console.WriteLine("Third Hello World!")
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CalculateAverage([1, 2, 3]));
+
+    }
+    static int CalculateAverage(int[] ints)
+    {
+        //return ints.Average();
+
+        int sum = 0;
+        for (int i = 0; i < ints.Length; i++)
+        {
+            sum += ints[i];
+        }
+
+        return sum / ints.Length;
+    }
+}

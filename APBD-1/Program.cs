da9bf23f7a200da498812fd9a69fa7077a606fe8
s30181 <s30181@pjwstk.edu.pl>
2025-03-03 18:09:06 +0100

    Modification 2

--- APBD-1/Program.cs
+++ APBD-1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("First Hello World!")
+Console.WriteLine("Second Hello World!")
